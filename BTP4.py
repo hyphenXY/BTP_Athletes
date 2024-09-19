1d25dc391b1e2e376ef9c0aaf5e22278ae573382
Shivam Yadav <88543186+hyphenXY@users.noreply.github.com>
2024-09-19 11:54:10 +0000

    7

--- BTP4.py
+++ BTP4.py
@@ -1,1 +1,7 @@
-# API Key = G8DJG324M57QK384ZFD2RXC6W
+import requests
+
+APIKEY= "G8DJG324M57QK384ZFD2RXC6W"
+
+url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Paris?key={APIKEY}"
+
+
