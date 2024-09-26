a75343716633e77e37ee9c4940ea093f9e64b98f
Deepanshu <deepanshu20051@iiitd.ac.in>
2024-09-26 12:44:55 +0530

    readme

--- BTP3.py
+++ BTP3.py
@@ -1,27 +1,91 @@
-# take data from the data.csv and find all cities
+# # take data from the data.csv and find all cities
+
+# import csv
+# import re
+
+# def main():
+#     cities=[]
+#     with open('Data.tsv','r') as f:
+#         reader = csv.reader(f, delimiter='\t')
+#         # skip 1st line
+#         next(reader)
+#         for row in reader:
+#             temp1=row[0].split('$')
+#             temp2=temp1[2].split(',')
+#             cities.append(temp2[4][1:])
+            
+            
+            
+#     print(cities)
+#     # giev it a csv, each city in a new line
+#     with open('Cities.csv','w') as f:
+#         writer = csv.writer(f)
+#         for city in cities:
+#             writer.writerow([city])
+    
+# if __name__ == '__main__':
+#     main()
+
+# import csv
+# import re
+
+# def main():
+#     city_date_pairs = []
+#     with open('Data.tsv', 'r') as f:
+#         reader = csv.reader(f, delimiter='\t')
+#         # skip 1st line
+#         next(reader)
+#         for row in reader:
+#             temp1 = row[0].split('$')
+#             date = temp1[1].strip()
+#             temp2 = temp1[2].split(',')
+#             # if len(temp2) >= 2:
+#             #     city = temp2[-2].strip()  # City is the second-to-last element
+#             #     city_date_pairs.append((date, city))
+#             city_date_pairs.append((date,temp2[4][1:]))
+            
+#     print(city_date_pairs)
+    
+#     # Write to a CSV file, with date and city in separate columns
+#     with open('Cities_and_Dates.csv', 'w', newline='') as f:
+#         writer = csv.writer(f)
+#         writer.writerow(['Date', 'City'])  # Header row
+#         for date, city in city_date_pairs:
+#             writer.writerow([date, city])
+
+# if __name__ == '__main__':
+#     main()
 
 import csv
 import re
 
 def main():
-    cities=[]
-    with open('Data.tsv','r') as f:
+    city_date_pairs = []
+    with open('Data.tsv', 'r') as f:
         reader = csv.reader(f, delimiter='\t')
         # skip 1st line
         next(reader)
         for row in reader:
-            temp1=row[0].split('$')
-            temp2=temp1[2].split(',')
-            cities.append(temp2[4][1:])
+            temp1 = row[0].split('$')
+            print(temp1)
+            # Extract and clean date
+            date = temp1[1].strip()
+            date = date.replace('*', '').strip()  # Remove asterisk if present
             
+            # Extract city (after 4th comma)
+            temp2 = temp1[2].split(',')
+            if len(temp2) >= 5:
+                city = temp2[4].strip()
+                city_date_pairs.append((date, city))
             
-            
-    print(cities)
-    # giev it a csv, each city in a new line
-    with open('Cities.csv','w') as f:
-        writer = csv.writer(f)
-        for city in cities:
-            writer.writerow([city])
+    print(city_date_pairs)
     
+    # Write to a CSV file, with date and city in separate columns
+    with open('Cities_and_Dates.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(['Date', 'City'])  # Header row
+        for date, city in city_date_pairs:
+            writer.writerow([date, city])
+
 if __name__ == '__main__':
     main()
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # BTP_Athletes
+
+As of now we have gathered the data from the sources whatever so required for the work.
+Second part of the work comes to suggest player which event to participate 
+How --
+input by user
+player name,player age,current rank,city name,event date
+output by system
+whether to participate in the city on the event date considering the category of the event 
+model work-
+We have players rank along with the weather conditions from a event
+now if we put all the information in a regression/classification model
+will try to find out whether the future participation in the event for a particular player 
+will be fruitful for a player or not.
