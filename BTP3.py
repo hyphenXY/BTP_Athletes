1ea03def76825436cfc6d447466b9c3c2bac6dbb
Shivam Yadav <88543186+hyphenXY@users.noreply.github.com>
2024-10-18 05:54:20 +0000

    25

--- BTP3.py
+++ BTP3.py
@@ -10,138 +10,59 @@ def main():
         # skip 1st line
         next(reader)
         count=0
-        # for row in reader:
-        #     count+=1
-        #     if count<=6885:
-        #         continue
-        #     if len(row) == 14:
-        #         place = row[0]
-        #         name = row[1]
-        #         dob = row[2]
-        #         nation = row[3]
-        #         score = row[4]
-        #         date = row[8]
-        #         resultscore = row[10]
-        #         # city=(row[-3])
-        #         city = (row[6])
-        #         # remove all the special characters from front and behind
-        #         city = re.sub(r"^\W+|\W+$", "", city)
-
-        #         lst.append([place, name, dob, nation,
-        #                    score, date, resultscore, city])
-        #     elif len(row) == 15:
-        #         place = row[0]
-        #         name = row[1]
-        #         dob = row[2]
-        #         nation = row[3]
-        #         score = row[4]
-        #         date = row[8]
-        #         resultscore = row[10]
-        #         # city=(row[-4])
-        #         city = (row[6])
-        #         # remove all the special characters from front and behind
-        #         city = re.sub(r"^\W+|\W+$", "", city)
-
-        #         lst.append([place, name, dob, nation,
-        #                    score, date, resultscore, city])
-        #     elif len(row) == 16:
-        #         place = row[0]
-        #         name = row[1]
-        #         dob = row[2]
-        #         nation = row[3]
-        #         score = row[4]
-        #         date = row[8]
-        #         resultscore = row[10]
-        #         # city=(row[-4])
-        #         city = (row[6])
-        #         # remove all the special characters from front and behind
-        #         city = re.sub(r"^\W+|\W+$", "", city)
-
-        #         lst.append([place, name, dob, nation,
-        #                    score, date, resultscore, city])
-        #     elif len(row) == 17:
-        #         place = row[0]
-        #         name = row[1]
-        #         dob = row[2]
-        #         nation = row[3]
-        #         score = row[4]
-        #         date = row[8]
-        #         resultscore = row[10]
-        #         # city=(row[-5])
-        #         city = (row[6])
-        #         # remove all the special characters from front and behind
-        #         city = re.sub(r"^\W+|\W+$", "", city)
-
-        #         lst.append([place, name, dob, nation,
-        #                    score, date, resultscore, city])
-        #     elif len(row) == 18:
-        #         place = row[0]
-        #         name = row[1]
-        #         dob = row[2]
-        #         nation = row[3]
-        #         score = row[4]
-        #         date = row[8]
-        #         resultscore = row[10]
-        #         # city=(row[-5])
-        #         city = (row[6])
-        #         # remove all the special characters from front and behind
-        #         city = re.sub(r"^\W+|\W+$", "", city)
-
-        #         lst.append([place, name, dob, nation,
-        #                    score, date, resultscore, city])
-        #     elif len(row) == 19:
-        #         place = row[0]
-        #         name = row[1]
-        #         dob = row[2]
-        #         nation = row[3]
-        #         score = row[4]
-        #         date = row[8]
-        #         resultscore = row[10]
-        #         # city=(row[-6])
-        #         city = (row[6])
-        #         # remove all the special characters from front and behind
-        #         city = re.sub(r"^\W+|\W+$", "", city)
-
-        #         lst.append([place, name, dob, nation,
-        #                    score, date, resultscore, city])
-        #     elif len(row) == 20:
-        #         place = row[0]
-        #         name = row[1]
-        #         dob = row[2]
-        #         nation = row[3]
-        #         score = row[4]
-        #         date = row[8]
-        #         resultscore = row[10]
-        #         # city=(row[-6])
-        #         city = (row[6])
-        #         # remove all the special characters from front and behind
-        #         city = re.sub(r"^\W+|\W+$", "", city)
-
-        #         lst.append([place, name, dob, nation,
-        #                    score, date, resultscore, city])
-
-        #     lengthdict.add(len(row))
-
         for row in reader:
             count+=1
-            if count<=6885:
-                continue
-            
+            if count==6885:
+                break
+            # if len(row) == 14:
             place = row[0]
             name = row[1]
             dob = row[2]
             nation = row[3]
             score = row[4]
-            date = (row[6].split(','))[2]
-            resultscore = row[8]
-            row6=row[6].split(',')
-            if len(row6)==5:
-                city = row6[-1]
-            elif len(row6)==6:
-                city = row6[-2]
-                
+            date = row[8]
+            resultscore = row[10]
+            # city=(row[-3])
+            if len(row) == 14:
+                city = row[-3]
+            elif len(row) == 15:
+                city=row[-4]
+            elif len(row) == 16:
+                city = row[-5]
+            elif len(row) == 17:
+                city = row[-5]
+            # remove all the special characters from front and behind
+            # if len(row6) == 5:
+            #     city = row6[-1]
+            # elif len(row6) == 6:
+            #     city = row6[-2]
+            city = re.sub(r"^\W+|\W+$", "", city)
+
             lst.append([place, name, dob, nation,
-                        score, date, resultscore, city])            
+                        score, date, resultscore, city])
+
+        #     lengthdict.add(len(row))
+
+        # for row in reader:
+        #     count+=1
+        #     if count<=6885:
+        #         continue
+            
+        #     place = row[0]
+        #     name = row[1]
+        #     dob = row[2]
+        #     nation = row[3]
+        #     score = row[4]
+        #     date = (row[6].split(','))[2]
+        #     resultscore = row[8]
+        #     row6=row[6].split(',')
+        #     if len(row6)==5:
+        #         city = row6[-1]
+        #     elif len(row6)==6:
+        #         city = row6[-2]
+                
+        #     lst.append([place, name, dob, nation,
+        #                 score, date, resultscore, city])            
     # for i in lst:
     #     print(i)
 
@@ -149,7 +70,7 @@ def main():
         print(i)
 
     # Write to a CSV file, with date and city in separate columns
-    with open('Cities_and_Dates.csv', 'w', newline='') as f:
+    with open('Cities_and_Dates1.csv', 'w', newline='') as f:
         writer = csv.writer(f)
         writer.writerow(['Place', 'Competitor', 'DOB',
                         'Nationality', 'Score', 'Date', 'ResultScore', 'City'])
--- BTP6.py
+++ BTP6.py
@@ -1,23 +1,38 @@
 # Import the required library
 from geopy.geocoders import Nominatim
-
-# Initialize Nominatim API
-geolocator = Nominatim(user_agent="MyApp")
-
-location = geolocator.geocode("delhi")
-
-print("The latitude of the location is: ", location.latitude)
-print("The longitude of the location is: ", location.longitude)
-
 import requests
 import json
 
+# Initialize Nominatim API
+
+cities = []
+with open('Cities_and_Dates2.csv') as f:
+    for line in f:
+        cities.append(line.split(',')[-1])
+
+for city in cities[1:]:
+    try:
+        geolocator = Nominatim(user_agent="MyApp")
+        print(city)
+        
+        location = geolocator.geocode(city)
+        
+        # print(location)
+        url = f"https://api.opentopodata.org/v1/srtm90m?locations={location.latitude},{location.longitude}&interpolation=cubic"
+        
+        response = requests.get(url)
+        data = response.json()
+        # add a column in the json
+        data['city'] = city
+        
+        # write a file
+        with open('elevationdata.json', 'a') as f:
+            json.dump(data, f)
+    except Exception as e:
+        continue
+
 #  api.opentopodata.org/v1/srtm90m?locations=-43.5,172.5|27.6,1.9&interpolation=cubic
 
-url = f"https://api.opentopodata.org/v1/srtm90m?locations={location.latitude},{location.longitude}&interpolation=cubic"
-
-response = requests.get(url)
-data = response.json()
 
 with open('elevationdata.json', 'w') as f:
     json.dump(data, f)
